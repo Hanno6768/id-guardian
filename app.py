d887609e29677437509e05a7fb10872e3197ae3d
Muhaned <muhanedmuaz@gmail.com>
2025-12-11 17:46:10 +0200

    deleted routes from app.py and moved idguardian.db from instance(deleted) to the parent folder

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
-from flask import Flask, render_template
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = '127.0.0.1:5000'
-app.config['APPLICATION_ROOT'] = '/'
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///idguardian.db")
 
 @app.route("/")
 def index():
     return render_template("layout.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # user reached here via POST
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # check for username and password
+        if not username:
+            flash("must provide username", "error")
+            return render_template("login.html")
+        elif not password:
+            flash("must provide password", "error")
+            return render_template("login.html")
+        
+        # username and password where provided
+        else:
+
+            # check if username and password match in database
+
+            # if ussername and password doesn't match take user back to the login.html
+            
+
+    else:
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
